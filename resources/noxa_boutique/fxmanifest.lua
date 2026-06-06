33ba00dbb913392a5d0d42e82b3318ba5920b9e6
gregstrl <greggpl76@gmail.com>
2026-06-06 03:16:44 +0200

    chore: retire noxa_boutique (boutique mise de côté pour le moment)

--- resources/noxa_boutique/fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-lua54 'yes'
-author 'NOXA FA'
-description 'NOXA FA — Boutique (NUI)'
-version '1.0.0'
-ui_page 'html/index.html'
-files { 'html/index.html' }
-client_script 'client.lua'
